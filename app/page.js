613e30b30fb2cfe515e3d6a27afa74049ebad838
Djinnx3 <alfonsolh1211@gmail.com>
2023-07-13 23:20:39 -0600

    Primer Commit proyecto de prueba

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    
+    <>
+    <h1>Wolis</h1>
+    
+    <h1 style={{ color: 'red' }} >Pepe</h1>
+    </>
+    
+  )
+}
